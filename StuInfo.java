fdb65f99f59bdb0f79af8613153a140505e8547f
Ahmed Saman <98406575+Ahmedsam199@users.noreply.github.com>
2022-04-21 19:55:20 +0300

    Update StuInfo.java

--- StuInfo.java
+++ StuInfo.java
@@ -2,6 +2,27 @@ public class StuInfo {
 String name;
 String PO;
 String Address;
+String Course1;
+public String getCourse1() {
+	return Course1;
+}
+public void setCourse1(String course1) {
+	Course1 = course1;
+}
+public String getCourse2() {
+	return Course2;
+}
+public void setCourse2(String course2) {
+	Course2 = course2;
+}
+public String getCourse3() {
+	return Course3;
+}
+public void setCourse3(String course3) {
+	Course3 = course3;
+}
+String Course2;
+String Course3;
 public String getName() {
 	return this.name;
 }
